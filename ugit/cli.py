cec6f26822fdec67bc25e041bfdcd525dd803a3d
Andrea Toaiari <andrea.toaiari@outlook.com>
2025-10-31 18:21:44 +0100

    added hash-object and cat-file commands

--- ugit/cli.py
+++ ugit/cli.py
@@ -1,25 +1,42 @@
 import argparse
 import os
+import sys
 
 from . import data
 
-def main ():
-    args = parse_args ()
-    args.func (args)
+def main():
+    args = parse_args()
+    args.func(args)
 
 
-def parse_args ():
-    parser = argparse.ArgumentParser ()
+def parse_args():
+    parser = argparse.ArgumentParser()
 
-    commands = parser.add_subparsers (dest='command')
+    commands = parser.add_subparsers(dest='command')
     commands.required = True
 
-    init_parser = commands.add_parser ('init')
-    init_parser.set_defaults (func=init)
+    init_parser = commands.add_parser('init')
+    init_parser.set_defaults(func=init)
+    
+    hash_object_parser = commands.add_parser('hash-object')
+    hash_object_parser.set_defaults(func=hash_object)
+    hash_object_parser.add_argument('file')
+    
+    cat_file_parser = commands.add_parser('cat-file')
+    cat_file_parser.set_defaults(func=cat_file)
+    cat_file_parser.add_argument('object')
 
-    return parser.parse_args ()
+    return parser.parse_args()
 
 
-def init (args):
+def init(args):
     data.init()
-    print (f'Initialized beautiful empty  ugit repository in {os.path.join(os.getcwd(), data.GIT_DIR)}')
+    print (f'Initialized empty ugit repository in {os.path.join(os.getcwd(), data.GIT_DIR)}')
+    
+def hash_object(args):
+    with open(args.file, 'rb') as f:
+        print(data.hash_object(f.read()))
+        
+def cat_file(args):
+    sys.stdout.flush()
+    sys.stdout.buffer.write(data.get_object(args.object))
--- ugit/data.py
+++ ugit/data.py
@@ -1,6 +1,18 @@
+import hashlib
 import os
 
 GIT_DIR = '.ugit'
 
-def init ():
-    os.makedirs (GIT_DIR)
+def init():
+    os.makedirs(GIT_DIR)
+    os.makedirs(os.path.join(GIT_DIR, 'objects'))
+    
+def hash_object(data):
+    oid = hashlib.sha1(data).hexdigest()
+    with open(os.path.join(GIT_DIR, 'objects', oid), 'wb') as out:
+        out.write(data)
+    return oid
+
+def get_object(oid):
+    with open(os.path.join(GIT_DIR, 'objects', oid), 'rb') as f:
+        return f.read()
