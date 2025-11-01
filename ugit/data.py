5fc79de56d40beb0ee3fa9ee88c1c96471e4c782
Andrea Toaiari <andrea.toaiari@outlook.com>
2025-11-01 20:17:03 +0100

    added parent commit logic

--- ugit/data.py
+++ ugit/data.py
@@ -10,12 +10,27 @@ def init():
     os.makedirs(GIT_DIR)
     os.makedirs(os.path.join(GIT_DIR, 'objects'))
     
+    
+def set_HEAD(oid):
+    with open(os.path.join(GIT_DIR, 'HEAD'), 'w') as f:
+        f.write(oid)
+        
+
+def get_HEAD():
+    head_path = os.path.join(GIT_DIR, 'HEAD')
+    if os.path.isfile(head_path):
+        with open(head_path) as f:
+            return f.read.strip()
+        
+        
+    
 def hash_object(data, type_='blob'):
     obj = type_.encode() + b'\x00' + data
     oid = hashlib.sha1(obj).hexdigest()
     with open(os.path.join(GIT_DIR, 'objects', oid), 'wb') as out:
         out.write(obj)
     return oid
+
 
 def get_object(oid, expected='blob'):
     with open(os.path.join(GIT_DIR, 'objects', oid), 'rb') as f:
