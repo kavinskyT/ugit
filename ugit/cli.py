d996c08bfa27bcce182bd8191ff4d687fe3a97d4
Andrea Toaiari <andrea.toaiari@outlook.com>
2025-11-16 10:59:25 +0100

    log: Show refs that point to each commit

--- ugit/cli.py
+++ ugit/cli.py
@@ -103,10 +103,15 @@ def commit(args):
     
 
 def log(args):
+    refs = {}
+    for refname, ref in data.iter_refs():
+        refs.setdefault(ref.value, []).append(refname)
+    
     for oid in base.iter_commits_and_parents({args.oid}):
         commit = base.get_commit(oid)
         
-        print(f'commit {oid}\n')
+        refs_str = f'({", ".join(refs[oid])})' if oid in refs else ''
+        print(f'commit {oid}{refs_str}')
         print(textwrap.indent(commit.message, '     '))
         print('')
         
@@ -120,7 +125,7 @@ def tag(args):
     
     
 # If no name is provided the function lists all the branches. 
-# Otherwise it creates the specified branch.
+# 00Otherwise it creates the specified branch.
 def branch(args):
     if not args.name:
         current = base.get_branch_name()
