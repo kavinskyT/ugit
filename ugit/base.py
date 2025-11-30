57f8b2090680ac1cfe18fd360e1a866a0b08502e
Andrea Toaiari <andrea.toaiari@outlook.com>
2025-11-30 12:43:21 +0100

    fetch: Download missing objects pointed by remote refs

--- ugit/base.py
+++ ugit/base.py
@@ -263,6 +263,9 @@ def get_commit(oid):
 
 
 def iter_commits_and_parents(oids):
+    # N.B. Must yield the oid before accessing it (to allow caller to fetch it 
+    # if needed)
+    
     # A deque is a double-endend queue. You can pop and append either from both ends.
     oids = deque(oids)
     visited = set()
@@ -281,7 +284,32 @@ def iter_commits_and_parents(oids):
         oids.extendleft(commit.parents[:1])
         # Return other parents later (first we want to run all the way to the root)
         oids.extend(commit.parents[1:])
-
+    
+        
+# It takes a list of commit OIDs and return all objects that are reachable from 
+# these commits.
+def iter_objects_in_commits(oids):
+    # N.B. Must yield the oid before accessing it (to allow caller to fetch it 
+    # if needed)
+    
+    visited = set()
+    def iter_objects_in_tree(oid):
+        visited.add(oid)
+        yield oid,
+        for type_, oid, _ in _iter_tree_entries(oid):
+            if oid not in visited:
+                if type_ == 'tree':
+                    yield from iter_objects_in_tree(oid)
+                else:
+                    visited.add(oid)
+                    yield(oid)
+    
+    for oid in iter_commits_and_parents(oids): 
+        yield oid
+        commit = get_commit(oid)
+        if commit.tree not in visited:
+            yield from iter_objects_in_tree(commit.tree)
+    
 
 def get_oid(name):
     if name == '@': name = 'HEAD'
--- ugit/remote.py
+++ ugit/remote.py
@@ -2,6 +2,7 @@
 
 import os
 
+from . import base
 from . import data
 
 
@@ -12,6 +13,10 @@ LOCAL_REFS_BASE = 'refs/remote/'
 def fetch(remote_path):
     # Gets refs from server
     refs = _get_remote_refs(remote_path, REMOTE_REFS_BASE)
+    
+    # Fetch missing objects by iterating and fetching on demand
+    for oid in base.iter_objects_in_commits(refs.values()):
+        data.fetch_object_if_missing(oid, remote_path)
     
     # Update local refs to match server
     for remote_name, value in refs.items():
