d71cf9a17cf0846a09e0506e9a1c53b46cda382b
Andrea Toaiari <andrea.toaiari@outlook.com>
2025-12-30 18:50:04 +0100

    diff: Add --cached option and take index into account

--- ugit/cli.py
+++ ugit/cli.py
@@ -61,7 +61,8 @@ def parse_args():
     
     diff_parser = commands.add_parser('diff')
     diff_parser.set_defaults(func=_diff)
-    diff_parser.add_argument('commit', default='@', type=oid, nargs='?')
+    diff_parser.add_argument ('--cached', action='store_true')
+    diff_parser.add_argument ('commit', nargs='?')
     
     checkout_parser = commands.add_parser('checkout')
     checkout_parser.set_defaults(func=checkout)
@@ -171,12 +172,28 @@ def show(args):
     sys.stdout.buffer.write(result)
         
 
-def _diff(args):
-    tree = args.commit and base.get_commit(args.commit).tree # var = var1 and var2 in Python uses the logical and operator, which evaluates to the first falsy value if one exists, otherwise it returns the last value.
-    
-    result = diff.diff_trees(base.get_tree(tree), base.get_working_tree())
-    sys.stdout.flush()
-    sys.stdout.buffer.write(result)
+def _diff (args):
+    oid = args.commit and base.get_oid (args.commit)
+
+    if args.commit:
+        # If a commit was provided explicitly, diff from it
+        tree_from = base.get_tree (oid and base.get_commit (oid).tree)
+
+    if args.cached:
+        tree_to = base.get_index_tree ()
+        if not args.commit:
+            # If no commit was provided, diff from HEAD
+            oid = base.get_oid ('@')
+            tree_from = base.get_tree (oid and base.get_commit (oid).tree)
+    else:
+        tree_to = base.get_working_tree ()
+        if not args.commit:
+            # If no commit was provided, diff from index
+            tree_from = base.get_index_tree ()
+
+    result = diff.diff_trees (tree_from, tree_to)
+    sys.stdout.flush ()
+    sys.stdout.buffer.write (result)
 
 
 def checkout(args):
